a24689125b422ae5b815d5127c9a6ca941d3029f
EDUARDO FONTANA GARBE <eduardo.fontana1099@univillebr.onmicrosoft.com>
2020-07-03 01:35:14 +0000

    funcionalidade de alterar e excluir

--- src/main/java/br/univille/agrotech/controller/FornecedorController.java
+++ src/main/java/br/univille/agrotech/controller/FornecedorController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,8 +28,19 @@ public class FornecedorController {
     }
 
     @GetMapping("/novo")
-    public ModelAndView novo(){
+    public ModelAndView novo(@ModelAttribute Fornecedor fornecedor){
         return new ModelAndView("fornecedor/form");
+    }
+
+    @PostMapping(params = "form")
+    public ModelAndView save(Fornecedor fornecedor){
+        service.save(fornecedor);;
+        return new ModelAndView("redirect:/fornecedor");
+    }
+
+    @GetMapping(value = "/alterar/{id}")
+    public ModelAndView edit(@PathVariable("id") Fornecedor fornecedor){
+        return new ModelAndView("fornecedor/form","fornecedor",fornecedor);
     }
     
 }
