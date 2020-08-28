02354961df650ae66d77276b50648f57602bcb96
EDUARDO FONTANA GARBE <eduardo.fontana1099@univillebr.onmicrosoft.com>
2020-08-28 01:20:26 +0000

    Segundo Update API

--- src/main/java/br/univille/agrotech/api/FornecedorControllerAPI.java
+++ src/main/java/br/univille/agrotech/api/FornecedorControllerAPI.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +30,30 @@ public class FornecedorControllerAPI {
         return new ResponseEntity<>(lista, HttpStatus.OK);
     }
 
-    @GetMapping(";{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Fornecedor> getById(@PathVariable("id") Fornecedor fornecedor){
+        return new ResponseEntity<>(fornecedor, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Fornecedor> save (@RequestBody Fornecedor fornecedor){
+        service.save(fornecedor);
+        return new ResponseEntity<>(fornecedor, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fornecedor> update (@PathVariable("id") Fornecedor fornecedorAntigo,
+                                              @RequestBody Fornecedor fornecedorAtualizado){
+        fornecedorAntigo.setNome(fornecedorAtualizado.getNome());
+        fornecedorAntigo.setTelefone(fornecedorAtualizado.getTelefone());
+        fornecedorAntigo.setEmail(fornecedorAtualizado.getEmail());
+        service.save(fornecedorAntigo);
+        return new ResponseEntity<>(fornecedorAntigo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Fornecedor> delete(@PathVariable("id") Fornecedor fornecedor){
+        service.delete(fornecedor);
         return new ResponseEntity<>(fornecedor, HttpStatus.OK);
     }
     
