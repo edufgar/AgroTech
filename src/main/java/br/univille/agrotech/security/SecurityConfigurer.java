2aadff466994b3735e89d1e8bd2b8ff2cfe0e5c7
EDUARDO FONTANA GARBE <eduardo.fontana1099@univillebr.onmicrosoft.com>
2020-09-18 01:11:06 +0000

    3 update security

--- src/main/java/br/univille/agrotech/security/SecurityConfigurer.java
+++ src/main/java/br/univille/agrotech/security/SecurityConfigurer.java
@@ -7,8 +7,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.univille.agrotech.service.impl.MyUserDetailsService;
  
@@ -16,6 +19,8 @@ import br.univille.agrotech.service.impl.MyUserDetailsService;
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
  
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -24,14 +29,20 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
      
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        // return NoOpPasswordEncoder.getInstance();
+        // https://en.wikipedia.org/wiki/Bcrypt
+        return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests().antMatchers("/api/v1/auth/signin").permitAll()
-            .anyRequest().authenticated();
+            //.anyRequest().authenticated()
+            .antMatchers("/api/**").authenticated()
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
              
     }
     @Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO FORNECEDOR(nome,telefone,email) values('Pedrinho','555-5555','pedrinho@pedrinho.com') ;
 INSERT INTO FORNECEDOR(nome,telefone,email) values('Zezinho','444-4444','zezinho@zezinho.com') ;
 
-insert IGNORE into usuario(id,usuario,senha) values(1,'admin','admin');
+-- insert IGNORE into usuario(id,usuario,senha) values(1,'admin','admin');
+
